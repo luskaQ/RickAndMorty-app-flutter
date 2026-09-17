6cf92d93ae26c02a49f849f43cdd98775c3c9a3b
luskaQ <lucasfpolidorio@gmail.com>
2026-09-17 11:36:35 -0300

    umas parada ai 2

--- lib/service/rickAndMortyService.dart
+++ lib/service/rickAndMortyService.dart
@@ -4,19 +4,37 @@ import 'package:http/http.dart' as http;
 class Rickandmortyservice {
   Future<Map> getPersonagens(String? busca) async {
     http.Response response;
-    response = await http.get(Uri.parse("https://rickandmortyapi.com/api/character"));
+    if (busca == null ||busca.isEmpty) {
+      response =
+      await http.get(Uri.parse("https://rickandmortyapi.com/api/character"));
+    } else{
+      response =
+      await http.get(Uri.parse("https://rickandmortyapi.com/api/character/?name=$busca"));
+    }
 
     return json.decode(response.body);
   }
   Future<Map> getLocais(String? busca) async {
     http.Response response;
-    response = await http.get(Uri.parse("https://rickandmortyapi.com/api/location"));
+    if (busca == null ||busca.isEmpty) {
+      response =
+      await http.get(Uri.parse("https://rickandmortyapi.com/api/location"));
+    } else{
+      response =
+      await http.get(Uri.parse("https://rickandmortyapi.com/api/location/?name=$busca"));
+    }
 
     return json.decode(response.body);
   }
   Future<Map> getEpisodios(String? busca) async {
     http.Response response;
-    response = await http.get(Uri.parse("https://rickandmortyapi.com/api/episode"));
+    if (busca == null ||busca.isEmpty) {
+      response =
+      await http.get(Uri.parse("https://rickandmortyapi.com/api/episode"));
+    } else{
+      response =
+      await http.get(Uri.parse("https://rickandmortyapi.com/api/episode/?name=$busca"));
+    }
 
     return json.decode(response.body);
   }
